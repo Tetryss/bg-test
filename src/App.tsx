76e9545708ed5de1ff2254ed7fbf1045d93a494b
Tetryss <jachumbre01@gmail.com>
2024-11-19 13:35:55 -0600

    Modular setting addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,122 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { Float, Environment, Dodecahedron } from "@react-three/drei";
-import { useRef, useState } from "react";
-import { Vector3 } from "three";
+import { Float, Environment } from "@react-three/drei";
+import { useControls } from "leva";
+import { useEffect, useRef, useState } from "react";
+const colors = ["#FCFC62", "#4062BB", "#F7A072", "#D90368"];
+const shapes = ["box", "sphere", "capsule", "capsule", "cone", "torus"];
 
 export default function App() {
-    const [mousePosition, setMousePosition] = useState(new Vector3());
-    const divRef = useRef<HTMLDivElement>(null);
+    const bgRef = useRef<HTMLDivElement | null>(null);
+    const { speed, spread, objectCount } = useControls({
+        blurMode: {
+            value: false,
+            onChange: (newValue) => {
+                if (bgRef.current) {
+                    bgRef.current.setAttribute(
+                        "data-blur",
+                        JSON.stringify(newValue)
+                    );
+                }
+            },
+        },
+        objectCount: {
+            value: 1,
+            min: 1,
+            max: 10,
+            step: 1,
+        },
+        speed: {
+            value: 1,
+            min: 1.1,
+            max: 6,
+            step: 0.05,
+        },
+        spread: {
+            value: 1,
+            min: 0,
+            max: 1.5,
+            step: 0.05,
+        },
+    });
 
-    const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-        if (divRef.current) {
-            const rect = divRef.current.getBoundingClientRect();
-            const x = ((event.clientX - rect.left) / rect.width) * 2 - 1; // Normalize X
-            const y = -((event.clientY - rect.top) / rect.height) * 2 + 1; // Normalize Y
-
-            // Map normalized coordinates to 3D space
-            setMousePosition(new Vector3(x * 5, y * 5, 1.5)); // Adjust scaling as needed
-        }
-    };
+    const [shapes, setShapes] = useState([
+        "box",
+        "sphere",
+        "capsule",
+        "capsule",
+        "cone",
+        "torus",
+    ]);
+    useEffect(() => {
+        const newShapes = fillRandomShapes(objectCount);
+        setShapes([]);
+        setShapes([...newShapes]);
+        setShapes;
+    }, [objectCount]);
 
     return (
         <>
             <div className="divvie">
                 <p>Skibidi</p>
             </div>
-            <div onMouseMove={handleMouseMove} id="SceneDiv" ref={divRef}>
+            <div ref={bgRef} id="SceneDiv">
                 <Canvas>
-                    <Float speed={1.2} rotationIntensity={9} floatIntensity={4}>
-                        <mesh position={[1.3, 0.1, -1]}>
-                            <boxGeometry args={[1.8, 1.8, 1.8]} />
-                            <meshPhongMaterial color="#fd19cc" />
-                        </mesh>
-                    </Float>
-
-                    <Float speed={1.5} floatIntensity={10}>
-                        <mesh>
-                            <sphereGeometry args={[1.3, 32, 32]} />
-                            <meshStandardMaterial color="#456990" />
-                        </mesh>
-                    </Float>
-                    <Float
-                        speed={1.2}
-                        rotationIntensity={5}
-                        floatIntensity={10}
-                    >
-                        <mesh position={[-1, 0, 1]}>
-                            <capsuleGeometry args={[0.5, 1.2, 2, 8]} />
-                            <meshStandardMaterial color="yellow" />
-                        </mesh>
-                    </Float>
-
-                    {/* Sphere following mouse position */}
-                    <mesh position={mousePosition.toArray()}>
-                        <sphereGeometry args={[0.3, 32, 32]} />
-                        <meshStandardMaterial color="black" />
-                    </mesh>
-
-                    <Float speed={2} rotationIntensity={5} floatIntensity={1}>
-                        <mesh position={[-0.5, 0, 0]}>
-                            <coneGeometry args={[1.8, 3, 16]} />
-                            <meshStandardMaterial color="#F45B69" />
-                        </mesh>
-                    </Float>
-                    <Float speed={3} rotationIntensity={5} floatIntensity={1}>
-                        <mesh position={[-1, 0, 0]}>
-                            <torusGeometry args={[1.2, 0.6, 16, 100]} />
-                            <meshStandardMaterial color="#363358" />
-                        </mesh>
-                    </Float>
-
-                    <Environment preset="apartment" />
+                    {shapes.map((key, index) => {
+                        const pos = (index - shapes.length / 2 + 0.1) * spread;
+                        const randomColor = getRandomColor();
+                        return (
+                            <Float
+                                speed={speed}
+                                rotationIntensity={Math.random() * 5}
+                                floatIntensity={5}
+                                key={index}
+                            >
+                                <mesh position={[pos, 0, 0]}>
+                                    {key === "box" && (
+                                        <boxGeometry args={[1.5, 1.5, 1.5]} />
+                                    )}
+                                    {key === "sphere" && (
+                                        <sphereGeometry args={[1.3, 32, 32]} />
+                                    )}
+                                    {key === "capsule" && (
+                                        <capsuleGeometry
+                                            args={[0.5, 1.2, 2, 8]}
+                                        />
+                                    )}
+                                    {key === "cone" && (
+                                        <coneGeometry args={[1.8, 3, 16]} />
+                                    )}
+                                    {key === "torus" && (
+                                        <torusGeometry
+                                            args={[1.2, 0.6, 16, 100]}
+                                        />
+                                    )}
+                                    <meshPhongMaterial color={randomColor} />
+                                </mesh>
+                            </Float>
+                        );
+                    })}
+                    <Environment preset="studio" />
+                    <ambientLight intensity={0.5} />
                     <directionalLight position={[0, 0, 1]} />
                 </Canvas>
             </div>
         </>
     );
 }
+
+const getRandomColor = (): string => {
+    const randNum = Math.floor(Math.random() * colors.length); // Fix random calculation
+    return colors[randNum];
+};
+
+const fillRandomShapes = (count: number): Array<string> => {
+    let retVal: Array<string> = [];
+    for (let i = 0; i < count; i++) {
+        // Fix increment
+        const randNum = Math.floor(Math.random() * shapes.length); // Fix random calculation
+        retVal.push(shapes[randNum]);
+    }
+    return retVal;
+};
